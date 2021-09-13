772c481212c7f8c48b679e62dc8b124c9bb69a2e
Sean Jin <seanhoyeonjin@gmail.com>
2021-09-13 21:48:53 +0000

    Adding Head to Lotide Challenge

--- head.js
+++ head.js
@@ -13,3 +13,11 @@ assertEqual(1, 1);
 assertEqual(1, 2);
 assertEqual("hello", "Hello");
 assertEqual("yoooo", "yoooo");
+
+const head = function(array) {
+  return array[0];
+}
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([]), 5);
