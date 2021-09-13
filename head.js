14f8c2ed156a78bf236f231eeaf3c2f0ffe78ffe
Sean Jin <seanhoyeonjin@gmail.com>
2021-09-13 22:13:14 +0000

    Added the Tail section of Lotide

--- head.js
+++ head.js
@@ -7,17 +7,11 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-//TEST  CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(1, 2);
-assertEqual("hello", "Hello");
-assertEqual("yoooo", "yoooo");
-
 const head = function(array) {
   return array[0];
 }
 
+//TEST CODE
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 assertEqual(head([]), 5);
---
+++ tail.js
@@ -0,0 +1,39 @@
+//FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`👍👍👍Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`⛔⛔⛔Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const tail = function(array) {
+  let newArray = [];
+  for (let i = 1; i < array.length; i++) {
+    newArray.push(array[i]);
+  }
+  return newArray;
+};
+
+// Test Case 1: Check the returned array elements
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+
+// Test Case 2: ...
+const result1 = tail([]);
+assertEqual(result1.length, 0); // ensure we get back two elements
+assertEqual(result1[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result1[1], "Labs"); // ensure second element is "Labs"
+
+// Test Case 3: ...
+const result2 = tail(["hello"]);
+assertEqual(result2.length, 0); // ensure we get back two elements
+assertEqual(result2[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result2[1], "Labs"); // ensure second element is "Labs"
+
+// Test Case: Check the original array 
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
