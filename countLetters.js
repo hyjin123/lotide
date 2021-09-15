688ca8516bc41032531d7ace44864042f186d6ba
Sean Jin <seanhoyeonjin@gmail.com>
2021-09-15 18:57:36 +0000

    Solutions to implement count letters

---
+++ countLetters.js
@@ -0,0 +1,30 @@
+//AssertEqual Function
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`👍👍👍Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`⛔⛔⛔Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+//this function should take in a sentence and then return
+// a count of each of the letters in that sentence
+const countLetters = function(sentence) {
+  let results = {};
+  for (const item of sentence) {
+    if (results[item]) {
+      results[item] += 1;
+    } else {
+      results[item] = 1;
+    }
+  }
+  return results;
+}
+
+//TEST CODE -- should pass
+let result = countLetters("LHL");
+assertEqual(result["L"], 2);
+
+let result1 = countLetters("lighthouse in the house");
+assertEqual(result1["h"], 4);
+assertEqual(result1["e"], 3);
